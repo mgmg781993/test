6405efc77a47aeebdd7a3b6e450408bf59f6b19a
mgmg781993 <myatnoe.aidma@gmail.com>
2018-03-29 16:43:07 +0630

    star rating

--- Screens/SecondScreen.js
+++ Screens/SecondScreen.js
@@ -12,36 +12,82 @@ import {
   View,
   ScrollView,
   Dimensions,
-  TextInput
+  TextInput,
+  Button,
+  Image,
+  Alert
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-import Swiper from 'react-native-swiper'
+import Swiper from 'react-native-swiper';
+import StarRating from 'react-native-star-rating';
 
 
 export default class SecondScreen extends Component {
   static navigationOptions ={
       title:'SecondScreen'
   };
+  constructor(props){
+    super(props);
+    this.state={
+  starCount:3.5
+    };
+  }
+
+  onStarRatingPress(rating) {
+    this.setState({
+      starCount: rating
+    });
+  }
+
   render() {
     return (
       <View style={styles.wrapper} >
       <View style={styles.swipwrapper}>
       <Swiper showsButtons={true} autoplay={true}>
         <View style={styles.slide1}>
-            <Text style={styles.text}>Hello Swiper</Text>
+        <Image
+       style={styles.stretch}
+       source={require('../images/pan_01.png')}
+     />
           </View>
           <View style={styles.slide2}>
-            <Text style={styles.text}>Beautiful</Text>
+          <Image
+         style={styles.stretch}
+         source={require('../images/pan_01.png')}
+       />
           </View>
           <View style={styles.slide3}>
-            <Text style={styles.text}>And simple</Text>
+          <Image
+         style={styles.stretch}
+         source={require('../images/pan_01.png')}
+       />
           </View>
       </Swiper>
           </View>
       <View style={styles.detail}>
-      <TextInput
-      placeholder='Search' />
-        </View>
+      <StarRating
+       fullStar={require('../images/full_star.png')}
+       emptyStar={require('../images/empty_star.png')}
+       halfStar={require('../images/star-half-empty.png')}
+       disabled={false}
+       maxStars={5}
+       rating={this.state.starCount}
+       selectedStar={(rating) => this.onStarRatingPress(rating)}
+     />
+     <Text style={styles.textDeatil}>
+    Tip
+     </Text>
+      <Text style={styles.textDeatil}>
+      All the useful tips and tricks have been gathered here -
+      there is a bunch of them, which makes this section a must-read
+      </Text>
+      </View>
+      <Button
+    onPress={() => {
+      Alert.alert('Success!');
+    }}
+    title="Buy Now"
+  />
       </View>
     );
   }
@@ -53,8 +99,13 @@ const styles = StyleSheet.create({
   swipwrapper: {
     height: (Dimensions.get('window').width/2)-2,
   },
-  Detail: {
-    height: (Dimensions.get('window').width/2)-2,
+  detail: {
+    margin: 10,
+  },
+  textDeatil:{
+     marginTop: 20,
+     color: '#000',
+     fontSize: 24,
   },
   slide1: {
     height: (Dimensions.get('window').width/2)-2,
